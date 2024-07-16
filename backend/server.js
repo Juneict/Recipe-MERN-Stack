0dedb6542a22880c1c752bdf745ed8586a82c387
Juneict <junemoenyinyiict@gmail.com>
2024-07-16 23:05:52 +0700

    add : mongo db setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
-
+const mongoUrl = "mongodb+srv://junemoenyinyiict:adminadmin@mern-cluster.e9gb9hh.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster";
+mongoose.connect(mongoUrl)
+    .then(() => {
+        console.log('Connected!');
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    });
 const RecipesRoutes = require('./router/recipes');
 
 const port = process.env.PORT;
@@ -13,8 +21,5 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.use('/api/recipes',RecipesRoutes);
+app.use('/api/recipes', RecipesRoutes);
 
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
-})
